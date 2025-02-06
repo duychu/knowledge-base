83a095aa29fbf3ad564cf74b724fecf548889ef0
Duy Chu <duycb92@gmail.com>
2025-02-06 10:35:34 +0000

    fix: build error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -109,7 +109,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/tutorials/intro',
             },
           ],
         },
